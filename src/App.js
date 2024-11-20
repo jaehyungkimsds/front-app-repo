8105cc9cdbbcadbe5dc1e07771a8bd7bf65ea585
jaehyungkimsds <jdjay.kim@samsung.com>
2024-11-20 08:12:23 +0000

    Add new blog version 3

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog version 3
+            EKS DEMO Blog version 3 real
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
